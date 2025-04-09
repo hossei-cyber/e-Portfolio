cc3cc075921f3fbd9703fa7d24cedd2e2e56c4a2
hossei-cyber <hussayaqobzai@gmail.com>
2025-04-09 21:58:54 +0200

    changed route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/getFood', {
+        const response = await axios.get('https://agreeable-pebble-034724710.6.azurestaticapps.net/getFood', {
           withCredentials: true,
         });
         console.log('Fetched data:', response.data); // Log the fetched data
