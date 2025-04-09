6c8acdfd0ff64ac5c00a5da7fc63957126247ca5
hossei-cyber <hussayaqobzai@gmail.com>
2025-04-09 21:31:30 +0200

    axios

--- server/routes/test.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const test = require('../controllers/test'); // Import your controller
-
-
-router.get('/getFood', test.getFood);
-
-module.exports = router;
